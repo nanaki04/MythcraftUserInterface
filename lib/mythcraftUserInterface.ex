c0f255891b64dc9680e51baad210211b359ebc26
Robert Jan Zwetsloot <zwetsloot.robert.jan@techcross.co.jp>
2017-04-13 09:39:09 +0900

    initialized the user interface module for the Mythcraft game

---
+++ lib/mythcraftUserInterface.ex
@@ -0,0 +1,18 @@
+defmodule MythcraftUserInterface do
+  @moduledoc """
+  Documentation for MythcraftUserInterface.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> MythcraftUserInterface.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/mythcraftUserInterface_test.exs
@@ -0,0 +1,8 @@
+defmodule MythcraftUserInterfaceTest do
+  use ExUnit.Case
+  doctest MythcraftUserInterface
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
